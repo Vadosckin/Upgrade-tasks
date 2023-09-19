e180f3b3d727f8acdbb0c9788eab0930e9dbc4e3
Vadosckin <vadick.pilipencko12@gmail.com>
2023-09-19 07:43:12 +0300

    modified 03 Variables and Data

---
+++ 03 Variables and Data Types/task2.js
@@ -0,0 +1,9 @@
+let dataFile = 'some data'
+console.log (dataFile)
+
+
+let nameFile = 'some name'
+console.log(nameFile)
+
+let errorsSave = 'some errors'
+console.log(errorsSave)
---
+++ 03 Variables and Data Types/task4.js
@@ -0,0 +1,26 @@
+let middle
+
+let productPrice1 = 101.5; // цена 1го продукта
+let productPrice2 = 50.25; // цена 2го продукта
+let productPrice3 = 10.11; // цена 3го продукта
+
+middle = (productPrice1 + productPrice2 + productPrice3)/3
+
+console.log (middle)
+
+
+let all
+
+let productQuantity1 = 5;  // количество единиц 1го продукат на складе
+let productQuantity2 = 9;  // количество единиц 2го продукат на складе
+let productQuantity3 = 15; // количество единиц 3го продукат на складе
+
+all = (productQuantity1 * productPrice1) + 
+(productQuantity2 * productPrice2) + (productQuantity3 * productPrice3)
+
+console.log(all)
+
+        
+        // Выведите на экран:
+        // 1) Среднюю цену на продукт
+        // 2) Общую стоимость всех продуктов, которые есть на складе
